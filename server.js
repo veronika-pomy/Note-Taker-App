bec5eec301fe47b831800351689d8e02472bbb77
Veronika Pomyateeva <pomyateevaav@gmail.com>
2023-02-12 19:10:30 -0500

    woring with get api/notes route to read json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // require express & path
 const express = require('express');
 const path = require ('path');
+const fs = require('fs');
 
 // require fs utilities modules
 const { readJsonFile, writeJsonFile, appendJsonFile } = require('./helper/fsUtils');
@@ -40,49 +41,34 @@ app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-
-
-
-
-
-
-
 // GET Route for reading and returning saved notes 
 app.get('/api/notes', (req, res) => {
     // using fs untility to read from json file 
-    readJsonFile('./db/db.json')
-    .then((data) => 
-        res.json(JSON.parse(data)),
-    );
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+      })  
 });
 
 // GET Route for a specific note by id
-app.get('api/notes/:id', (req, res) => {
-    const noteId = req.params.note_id;
-    readJsonFile('./db/db.json')
-      .then((data) => JSON.parse(data))
-      .then((json) => {
-        const result = json.filter((note) => note.note_id === noteId);
-        return result.length > 0
-          ? res.json(result)
-          : res.json('No note with that ID');
-      });
-  });
+// app.get('api/notes/:id', (req, res) => {
+//     const noteId = req.params.note_id;
+//     readJsonFile('./db/db.json')
+//       .then((data) => JSON.parse(data))
+//       .then((json) => {
+//         const result = json.filter((note) => note.note_id === noteId);
+//         return result.length > 0
+//           ? res.json(result)
+//           : res.json('No note with that ID');
+//       });
+//   });
 
-
-
-
-
-
-
-
-  
 // POST Route for a new note ot be added to json file with saved notes
     // add unique id to each note 
 app.post('/api/notes', (req, res) => {
 
     // destructure the requested obj first
-    const { title, text, note_id } = req.body;
+    const { title, text } = req.body;
 
     // check that the note to be saved has a title before making a new note
     if (title) {
@@ -110,22 +96,22 @@ app.post('/api/notes', (req, res) => {
 });
 
 // DELETE Route for specific note by id
-app.delete('api/notes/:id', (req, res) => {
-    const noteId = req.params.note_id;
-    readJsonFile('./db/db.json')
-      .then((data) => JSON.parse(data))
-      .then((json) => {
+// app.delete('api/notes/:id', (req, res) => {
+//     const noteId = req.params.note_id;
+//     readJsonFile('./db/db.json')
+//       .then((data) => JSON.parse(data))
+//       .then((json) => {
 
-        // filter notes array and exclude the one to be deleted by id
-        const result = json.filter((note) => note.note_id !== noteId);
+//         // filter notes array and exclude the one to be deleted by id
+//         const result = json.filter((note) => note.note_id !== noteId);
   
-        // write the new array to the json file 
-        writeJsonFile('./db/db.json', result);
+//         // write the new array to the json file 
+//         writeJsonFile('./db/db.json', result);
   
-        // response
-        res.json(`Note with ID ${noteId} has been deleted 🗑️`);
-    });
-});
+//         // response
+//         res.json(`Note with ID ${noteId} has been deleted 🗑️`);
+//     });
+// });
 
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT} 🚀`)
